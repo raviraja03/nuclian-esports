c6e1522e25d99ead208572c17d266cae67478745
Raja <97151004+raviraja03@users.noreply.github.com>
2025-09-23 19:30:38 +0530

    vite config file updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react()],
+    plugins: [tailwindcss(), react()],
     server: {
       port: parseInt(env.VITE_PORT) || 3000,
       host: true, // allows external access
