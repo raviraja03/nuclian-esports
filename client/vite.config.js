80a42537d90515d4a20100dbaa419633e7850f44
Raja <97151004+raviraja03@users.noreply.github.com>
2025-09-23 18:26:34 +0530

    vite config file update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  build: {
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true, // removes console.log/error/warn in prod
-      },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT) || 3000,
+      host: true, // allows external access
+      allowedHosts: ['www.tribexesports.com'], // whitelist your host
     },
-  },
-
-  base: "/", // Set base to root for proper routing
+    preview: {
+      port: parseInt(env.VITE_PORT) || 5000,
+      host: true,
+    },
+  };
 });
